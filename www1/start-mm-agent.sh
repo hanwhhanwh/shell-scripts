a04584293a1ea150e3e6b8c44d95fec695d49f61
Wonhee <hanwhhanwh@gmail.com>
2021-07-30 16:47:49 +0900

    add mm-agent start script

---
+++ www1/start-mm-agent.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# mm-agent 컨테이너 실행 스크립트
+# server : www1
+# date : 2021-07-30
+# author : hbesthee@naver.com
+
+LOG_MM_AGENT=/share/logs/www.log
+AGENT_ROOT_DIR=/share/serivce/mm-agent
+AGENT_PORT=13000
+
+echo "========" | tee -a ${LOG_MM_AGENT}
+echo "[$(date +%Y-%m-%d) $(date +%H:%M:%S)] $0 $@" | tee -a ${LOG_MM_AGENT}
+
+echo "mm-agent 컨테이너 생성" | tee -a ${LOG_MM_AGENT}
+#	-p ${AGENT_PORT}:3000 \
+# docker run -d --restart=unless-stopped --name homepage \
+docker run -d --network host --restart=unless-stopped --name mm-agent \
+	-e SERVICE_PORT=${AGENT_PORT} \
+	-v ${AGENT_ROOT_DIR}:/app/public \
+	hanwhhanwh/node:latest 2>&1 | tee -a ${LOG_MM_AGENT}
