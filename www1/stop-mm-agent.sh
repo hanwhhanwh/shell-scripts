c50ac05aed4a926db1a36d4133120240d08e3012
Wonhee <hanwhhanwh@gmail.com>
2021-08-21 15:22:43 +0900

    mm-agent stop script

---
+++ www1/stop-mm-agent.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# mm-agent 컨테이너 중지 스크립트
+# server : www1
+# date : 2021-08-21
+# author : hbesthee@naver.com
+
+LOG_MM_AGENT=/share/logs/mm-agent.log
+AGENT_ROOT_DIR=/share/service/mm_agent
+AGENT_PORT=13000
+
+echo "========" | tee -a ${LOG_MM_AGENT}
+echo "[$(date +%Y-%m-%d) $(date +%H:%M:%S)] $0 $@" | tee -a ${LOG_MM_AGENT}
+
+echo "mm-agent 컨테이너 중지" | tee -a ${LOG_MM_AGENT}
+docker stop mm-agent ; docker rm mm-agent | tee -a ${LOG_MM_AGENT}
