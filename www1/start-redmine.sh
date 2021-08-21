22c67a1ea23289690f97c6392481ab9dd199ba2b
Wonhee <hanwhhanwh@gmail.com>
2021-08-21 19:56:51 +0900

    change redmine log

--- www1/start-redmine.sh
+++ www1/start-redmine.sh
@@ -5,7 +5,7 @@
 # date : 2021-08-21
 # author : hbesthee@naver.com
 
-REDMINE_LOGS=/home/logs/redmine.log
+LOG_REDMINE=/home/logs/redmine.log
 REDMINE_FILES=/home/redmine/files
 REDMINE_LOGS=/home/redmine/logs
 REDMINE_PORT=3000
@@ -16,13 +16,13 @@ REDMINE_DB_PASSWORD=redmine2019!@
 REDMINE_DB_DATABASE=redmine
 
 
-echo "========" | tee -a ${REDMINE_LOGS}
-echo "[$(date +%Y-%m-%d) $(date +%H:%M:%S)] $0 $@" | tee -a ${REDMINE_LOGS}
+echo "========" | tee -a ${LOG_REDMINE}
+echo "[$(date +%Y-%m-%d) $(date +%H:%M:%S)] $0 $@" | tee -a ${LOG_REDMINE}
 
-echo "redmine 컨테이너 중지" | tee -a ${REDMINE_LOGS}
+echo "redmine 컨테이너 중지" | tee -a ${LOG_REDMINE}
 docker stop redmine ; docker rm redmine
 
-echo "redmine 컨테이너 생성" | tee -a ${REDMINE_LOGS}
+echo "redmine 컨테이너 생성" | tee -a ${LOG_REDMINE}
 	# -e REDMINE_NO_DB_MIGRATE=1 \
 	# -p {REDMINE_PORT}:3000 \
 docker run -d --network host --restart=unless-stopped --name redmine \
@@ -32,4 +32,5 @@ docker run -d --network host --restart=unless-stopped --name redmine \
 	-e REDMINE_DB_PASSWORD=${REDMINE_DB_PASSWORD} \
 	-e REDMINE_DB_DATABASE=${REDMINE_DB_DATABASE} \
 	-v ${REDMINE_FILES}:/usr/src/redmine/files \
-	redmine:4.2.2-alpine 2>&1 | tee -a ${REDMINE_LOGS}
+	-v ${REDMINE_LOGS}:/usr/src/redmine/log \
+	redmine:4.2.2-alpine 2>&1 | tee -a ${LOG_REDMINE}
