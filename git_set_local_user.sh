ee2657fbdf79c0e739c417397540fd2043148855
Wonhee Han <hanwhhanwh@gmail.com>
2021-09-25 10:56:20 +0900

    add git script : set user info to local

---
+++ git_set_local_user.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# @ 현 저장소의 user.name 및 user.email을 설정합니다.
+# @author hbesthee@naver.com
+# @date 2021-09-25
+
+git config --local user.email hanwhhanwh@gmail.com
+git config --local user.name "Wonhee Han"
