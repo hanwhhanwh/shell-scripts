ff5dbfdc00bfc6407cbf641a0d5d3273d03dec99
Wonhee Han <hanwhhanwh@gmail.com>
2026-06-22 09:28:27 +0900

    MOD: 새 창으로 클릭 링크 열기

--- tamper/suto_clicker.js
+++ tamper/suto_clicker.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         SuperToday Auto Clicker
 // @namespace    http://tampermonkey.net/
-// @version      0.3
-// @description  이벤트 자동 응모하기 - bo_v_img 내 naver.com 링크 클릭 (5초 간격)
+// @version      0.4
+// @description  이벤트 자동 응모하기 - bo_v_img 내 naver.com 링크 새 창 열기 (5초 간격)
 // @author       hbesthee@naver.com
 // @match        https://www.suto.co.kr/ztech/*
 // @grant        GM_setValue
@@ -20,7 +20,7 @@
 	let clickCount = 0;
 	const allClicks = [];
 
-	// Collect all links to click
+	// Collect all links to open
 	boVImgElements.forEach(imgElement => {
 		const aTags = imgElement.querySelectorAll('a');
 		aTags.forEach(aTag => {
@@ -31,24 +31,24 @@
 		});
 	});
 
-	// Click links one by one with 5 second intervals
+	// Open links in new tab one by one with 5 second intervals
 	let currentIndex = 0;
 
-	function clickNext() {
+	function openNext() {
 		if (currentIndex < allClicks.length) {
 			const aTag = allClicks[currentIndex];
-			aTag.click();
+			window.open(aTag.href, '_blank');
 			clickCount++;
 			currentIndex++;
 
-			setTimeout(clickNext, 1000);
+			setTimeout(openNext, 1000);
 		} else {
-			alert(`${clickCount}개의 링크를 클릭했습니다.`);
+			alert(`${clickCount}개의 링크를 새 창에서 열었습니다.`);
 		}
 	}
 
 	if (allClicks.length > 0) {
-		clickNext();
+		openNext();
 	} else {
 		alert('클릭할 링크가 없습니다.');
 	}
